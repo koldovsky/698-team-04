804c7b355124afe8cd3309914c65cf21521edf45
Semkovych <isemkovich39@gmail.com>
2022-07-10 09:42:44 +0000

    fixed media

--- js/friday.js
+++ js/friday.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const deadline = new Date(2022, 06, 08);
+    const deadline = new Date(2022, 06, 15);
     let timerId = null;
     function declensionNum(num, words) {
       return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
